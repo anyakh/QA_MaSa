77a7e0c9d693c99899d5b867caf19015cc55707a
Anna <annah2002@gmail.com>
2023-07-12 14:29:25 +0300

    lesson 1

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+       /*System.out.println("It's all about quality of life\r\n" + //
+               "          and finding a happy balance between work\r\n" + //
+               "                    and friends and family.");
+                int x=1;
+                x=x+1;
+               System.out.println("x="+x);
+               byte b,q;
+               b = -128;
+               q = 127;
+               System.out.println("byte b = "+b+" byte q = "+q);
+               short s,t;
+               s = -32768;
+               t = 32767;
+               System.out.println("short = "+s+" byte t = "+t);
+               long l =1000000000, m=10;
+               System.out.println("long l" +l);
+               String H = "hello";
+               System.out.println(H);
+               */
+              int q=3, p = 10;
+              System.out.println(p/q +" "+ p%3+"/"+q);
+   }
+}
