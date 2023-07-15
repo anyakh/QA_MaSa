58902291e8f9b5f83bbb1ae9c1abfa8e08c4cd70
Anna <annah2002@gmail.com>
2023-07-15 21:24:51 +0300

    home work with 3 numbers and average

---
+++ HW_1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class HW_1 {
+    public static void main(String[] args) {
+        System.out.println("Enter 3 numbers");
+        Scanner n1_scan=new Scanner(System.in);
+        int n1 = n1_scan.nextInt();
+        Scanner n2_scan=new Scanner(System.in);
+        int n2 = n2_scan.nextInt();
+        Scanner n3_scan=new Scanner(System.in);
+        int n3 = n3_scan.nextInt();
+        if(n1<n2){
+            if(n2<n3){
+                System.out.println(n3 +" > "+n2+" > "+n1);
+            }
+            else if(n2>n3){
+                if(n3>n1){
+                System.out.println(n2 +" > "+n3+" > "+n1);}
+                else if(n3<n1){
+                System.out.println(n2 +" > "+n1+" > "+n3);
+            }
+            }
+        }
+        else if(n2<n1){
+            if(n1<n3){
+                System.out.println(n3 +" > "+n1+" > "+n2); }
+            else if(n1>n3){
+                if(n3>n2){
+                    System.out.println(n1 +" > "+n3+" > "+n2);}
+                else if(n3<n2){
+                    System.out.println(n1 +" > "+n2+" > "+n3);
+                }
+            }
+        }
+        int average = (n1+n2+n3)/3;
+        System.out.println("average of "+ n1 + " " + n2 + " "+ n3 + " is " + average);
+        n1_scan.close();
+        n2_scan.close();
+        n3_scan.close();
+    }
+}
