16b2541f7d0e19913ed3530e608d85d6fc809b88
Anna <annah2002@gmail.com>
2023-07-31 14:17:39 +0300

    redone2.2

--- HW_3.java
+++ HW_3.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
 public class HW_3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
+        String c = "Choose the task wich you want to play: \n1st task: To count rectangle's square \n2nd task: To count factorial of the number";
+        int n = scan(c);
+        while (n>2 || n<1){
+            n = scan(c);
+        }
+        choise(n);
+   
+    }
+    static void choise(int x){
         
-//     Напишите программу, которая принимает два числа - длину и ширину прямоугольника от пользователя. 
-// Затем программа должна вызывать метод, который принимает эти два числа и возвращает 
-// площадь прямоугольника. 
+        switch(x) {
+            case 1: task1(); break;
+            case 2: task2(); break;
+        }
+    }
+    static void task2() {
 
-    task1();
-
-        // ØНапишите программу, которая запрашивает у пользователя целое неотрицательное число n
+// ØНапишите программу, которая запрашивает у пользователя целое неотрицательное число n
 //  и вычисляет его факториал. Факториал числа n обозначается как n! и равен произведению всех 
 // положительных целых чисел от 1 до n. Например, 5! = 5 * 4 * 3 * 2 * 1 = 120.
 //  Вам необходимо создать метод с именем factorial, который принимает целое число n в качестве аргумента
 //  и возвращает его факториал.
 
-    // task2();
-    }
-    static void task2() {
         String c = "Enter integer not negative number please: "; //задание 2
         int number = scan(c);
         while(number <= 0) {
@@ -26,6 +33,11 @@ public class HW_3 {
         printing(number, fac);
     }
     static void task1() {
+
+// Напишите программу, которая принимает два числа - длину и ширину прямоугольника от пользователя. 
+// Затем программа должна вызывать метод, который принимает эти два числа и возвращает 
+// площадь прямоугольника.
+
         String c = "Enter rectangle's parameter please:"; //задание 1 
         int length = scan(c);
         int width = scan(c);
